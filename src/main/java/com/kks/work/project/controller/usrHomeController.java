81c130682e5e838356f7468aabc5a530eef23c23
Oyat0824 <langue0824@gmail.com>
2023-01-09 20:24:01 +0900

    팀 프로젝트 스프링부트 환경 세팅

--- src/main/java/com/kks/work/project/controller/usrHomeController.java
+++ src/main/java/com/kks/work/project/controller/usrHomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class usrHomeController {
+public class UsrHomeController {
 	@RequestMapping("/usr/home/main")
 	@ResponseBody
 	public String showMain() {
